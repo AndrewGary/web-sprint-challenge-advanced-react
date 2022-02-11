184929f39c2ba37e8108db08faa47018fd8cc613
Andrew Gary <andrew.gary91@gmail.com>
2022-02-11 13:20:15 -0600

    working out all tests, app functioning

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,13 +1,187 @@
 import React from 'react'
+import axios from 'axios';
 
+const initialState = {
+  error: '',
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: ''
+}
 export default class AppClass extends React.Component {
+ state = initialState;
+
+  setActive = (x, y) => {
+    const grid = document.getElementsByClassName('square');
+
+    for(let i = 0; i < grid.length; i++){
+      if(grid[i].classList.contains('active')){
+        grid[i].classList.remove('active');
+      }
+      if(grid[i].textContent){
+        grid[i].textContent = '';
+      }
+    }
+
+    if(this.state.x === 1 && this.state.y === 1){
+      grid[0].classList.add('active')
+      grid[0].textContent = 'B'
+    }
+    if(this.state.x === 2 && this.state.y === 1){
+      grid[1].classList.add('active')
+      grid[1].textContent = 'B'
+    }
+    if(this.state.x === 3 && this.state.y === 1){
+      grid[2].classList.add('active')
+      grid[2].textContent = 'B'
+    }
+    if(this.state.x === 1 && this.state.y === 2){
+      grid[3].classList.add('active')
+      grid[3].textContent = 'B'
+    }
+    if(this.state.x === 2 && this.state.y === 2){
+      grid[4].classList.add('active')
+      grid[4].textContent = 'B'
+    }
+    if(this.state.x === 3 && this.state.y === 2){
+      grid[5].classList.add('active')
+      grid[5].textContent = 'B'
+    }
+    if(this.state.x === 1 && this.state.y === 3){
+      grid[6].classList.add('active')
+      grid[6].textContent = 'B'
+    }
+    if(this.state.x === 2 && this.state.y === 3){
+      grid[7].classList.add('active')
+      grid[7].textContent = 'B'
+    }
+    if(this.state.x === 3 && this.state.y === 3){
+      grid[8].classList.add('active')
+      grid[8].textContent = 'B'
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.x !== this.state.x ||
+       prevState.y !== this.state.y){
+         this.setActive(this.state.x, this.state.y);
+       }
+  }
+
+  handleUpClick = () => {
+    console.log('handleUpClick');
+    if(this.state.y === 1){
+      this.setState({
+        ...this.state,
+        error: "You can't go up"
+      })
+    }else{
+      this.setState({
+        ...this.state,
+        error: '',
+        y: this.state.y -1,
+        steps: this.state.steps+1
+      })
+    }
+  }
+
+  handleDownClick = () => {
+    console.log('handleDownClick');
+    if(this.state.y === 3){
+      this.setState({
+        ...this.state,
+        error: "You can't go down"
+      })
+    }else{
+      this.setState({
+        ...this.state,
+        error: '',
+        y: this.state.y +1,
+        steps: this.state.steps+1
+      })
+    }
+  }
+
+  handleLeftClick = () => {
+    console.log('handleLeftClick');
+    if(this.state.x === 1){
+      this.setState({
+        ...this.state,
+        error: "You can't go left"
+      })
+    }else{
+      this.setState({
+        ...this.state,
+        error: '',
+        x: this.state.x -1,
+        steps: this.state.steps+1
+      })
+    }
+  }
+
+  handleRightClick = () => {
+    console.log('handleRightClick');
+    if(this.state.x === 3){
+      this.setState({
+        ...this.state,
+        error: "You can't go right"
+      })
+    }else{
+      this.setState({
+        ...this.state,
+        error: '',
+        x: this.state.x +1,
+        steps: this.state.steps+1
+      })
+    }
+  }
+
+  handleReset = () => {
+    console.log('handleReset');
+    this.setState(initialState);
+  }
+
+  handleChange = e => {
+    this.setState({
+      ...this.state,
+      email: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('handleSubmit');
+
+
+
+    axios.post('http://localhost:9000/api/result', { x: this.state.x, y: this.state.y, steps: this.state.steps, email: this.state.email })
+    .then(resp => {
+      console.log('resp: ', resp)
+      this.setState({
+        ...this.state,
+        error: resp.data.message,
+        email: ''
+      })
+    })
+    .catch(error => {
+      console.log('error: ', error);
+      this.setState({
+        ...this.state,
+        error: `${this.state.email} failure #23`,
+        email: ''
+      })
+    })
+
+  }
+
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -21,18 +195,18 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.error}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.handleLeftClick} id="left">LEFT</button>
+          <button onClick={this.handleUpClick} id="up">UP</button>
+          <button onClick={this.handleRightClick} id="right">RIGHT</button>
+          <button onClick={this.handleDownClick} id="down">DOWN</button>
+          <button onClick={this.handleReset} id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input onChange={this.handleChange} id="email" type="email" placeholder="type email"></input>
+          <input onClick={this.handleSubmit} id="submit" type="submit"></input>
         </form>
       </div>
     )
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,169 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const initialState = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: 'gary91@gmail.com'
+}
 
 export default function AppFunctional(props) {
+  
+  const [ board, setBoard] = useState(initialState)
+
+  const [ error, setError ] = useState('');
+
+  const setActive = (x, y) => {
+    const grid = document.getElementsByClassName('square');
+
+    for(let i = 0; i < grid.length; i++){
+      if(grid[i].classList.contains('active')){
+        grid[i].classList.remove('active');
+      }
+      if(grid[i].textContent){
+        grid[i].textContent = '';
+      }
+    }
+
+    if(x === 1 && y === 1){
+      grid[0].classList.add('active')
+      grid[0].textContent = 'B'
+    }
+    if(x === 2 && y === 1){
+      grid[1].classList.add('active')
+      grid[1].textContent = 'B'
+    }
+    if(x === 3 && y === 1){
+      grid[2].classList.add('active')
+      grid[2].textContent = 'B'
+    }
+    if(x === 1 && y === 2){
+      grid[3].classList.add('active')
+      grid[3].textContent = 'B'
+    }
+    if(x === 2 && y === 2){
+      grid[4].classList.add('active')
+      grid[4].textContent = 'B'
+    }
+    if(x === 3 && y === 2){
+      grid[5].classList.add('active')
+      grid[5].textContent = 'B'
+    }
+    if(x === 1 && y === 3){
+      grid[6].classList.add('active')
+      grid[6].textContent = 'B'
+    }
+    if(x === 2 && y === 3){
+      grid[7].classList.add('active')
+      grid[7].textContent = 'B'
+    }
+    if(x === 3 && y === 3){
+      grid[8].classList.add('active')
+      grid[8].textContent = 'B'
+    }
+  }
+
+  useEffect(() => {
+    setActive(board.x, board.y)
+  }, [board])
+  
+  const handleUpClick = () => {
+
+    if(board.y === 1){
+      setError("You can't go up");
+    }else{
+      if(error){
+        setError('');
+      }
+      setBoard({...board,
+        y: board.y -1,
+        steps: board.steps + 1
+      })
+    }
+  }
+
+  const handleDownClick = () => {
+
+    if(board.y === 3){
+      setError("You can't go down");
+    }else{
+      if(error){
+        setError('');
+      }
+      setBoard({...board,
+        y: board.y +1,
+        steps: board.steps + 1
+      })
+    }
+  }
+
+  const handleRightClick = () => {
+
+    if(board.x === 3){
+      setError("You can't go right");
+    }else{
+      if(error){
+        setError('');
+      }
+      setBoard({...board,
+        x: board.x +1,
+        steps: board.steps + 1
+      })
+    }
+  }
+
+  const handleLeftClick = () => {
+    console.log('handleLeftClick');
+    if(board.x === 1){
+      setError("You can't go left");
+    }else{
+      if(error){
+        setError('');
+      }
+      setBoard({...board,
+        x: board.x -1,
+        steps: board.steps + 1
+      })
+    }
+  }
+
+  const handleResetClick = () => {
+    console.log('handleResetClick');
+    if(error){
+      setError('')
+    }
+    setBoard(initialState);
+  }
+  
+  const handleChange = (e) => {
+    setBoard({
+      ...board,
+      email: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('handleSubmit');
+
+    axios.post('http://localhost:9000/api/result', board)
+    .then(resp => {
+      console.log('board: ', board)
+      console.log(resp.data.message);
+      setError(resp.data.message);
+      
+    })
+    .catch(error => {
+      setError(`${board.email} failure #23`)
+    })
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates: ({board.x}, {board.y})</h3>
+        <h3 id="steps">You moved {board.steps} time</h3>
       </div>
       <div id="grid">
         <div className="square"></div>
@@ -19,18 +177,18 @@ export default function AppFunctional(props) {
         <div className="square"></div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{error}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={handleLeftClick}>LEFT</button>
+        <button id="up" onClick={handleUpClick}>UP</button>
+        <button id="right" onClick={handleRightClick}>RIGHT</button>
+        <button id="down" onClick={handleDownClick}>DOWN</button>
+        <button id="reset" onClick={handleResetClick}>reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input onChange={handleChange} id="email" type="email" placeholder="type email"></input>
+        <input id="submit" type="submit" onClick={handleSubmit}></input>
       </form>
     </div>
   )
